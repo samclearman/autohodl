2d837df76c4a7b10f9201b19a3b65f220839cb2f
Sam Clearman <sam@scale.com>
2021-11-28 16:56:31 -0800

    hold times

--- contracts/autohodl.sol
+++ contracts/autohodl.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 contract Autohodl
 {
   mapping(address => uint) private balances;
-  mapping(address => uint) private lockTimes;
-  
+  mapping(address => uint) private lockedUntil;
+
   constructor()
   {
   }
@@ -12,15 +12,25 @@ contract Autohodl
   /**
    * @dev Deposits some eth.
    */
-  function hodl(address hodler) payable public {
-    lockTimes[hodler] = block.timestamp;
+  function hodl(address hodler, uint lockSeconds) payable public {
+    lockedUntil[hodler] = block.timestamp + lockSeconds;
     balances[hodler] = balances[hodler] + msg.value;
+  }
+
+  function _isLocked(address hodler) private returns (bool) {
+    if (block.timestamp <= lockedUntil[hodler]) {
+      return true;
+    }
+    return false;
   }
 
   /**
    * @dev Withdraws the balance
    */
   function withdraw(address to) public {
+      if (_isLocked(to)) {
+          return;
+      }
     uint v = balances[to];
     balances[to] = 0;
     (bool success, ) = to.call{value: v}("");
--- test/autohodl.js
+++ test/autohodl.js
@@ -11,12 +11,14 @@ describe("Autohodl contract", function () {
 
     const hodlerAddr = await hodler.getAddress();
     const hodlValue = 1234;
-    await autohodlContract.connect(hodler).hodl(hodlerAddr, { value: hodlValue });
+    const hodlTime = 3;
+
+    await autohodlContract.connect(hodler).hodl(hodlerAddr, hodlTime, { value: hodlValue });
     const contractBalance = await provider.getBalance(autohodlContract.address);
     expect(contractBalance.toNumber()).to.equal(hodlValue)
   });
 
-  it("Should return money", async function () {
+  it("Shouldn't return money immediately", async function () {
     const [owner, hodler] = await ethers.getSigners();
 
     const autohodlFactory = await ethers.getContractFactory("Autohodl");
@@ -24,9 +26,29 @@ describe("Autohodl contract", function () {
 
     const hodlerAddr = await hodler.getAddress();
     const hodlValue = 12345;
+    const hodlTime = 3;
 
     const balancePre = await provider.getBalance(hodlerAddr);
-    const r1 = await (await autohodlContract.connect(hodler).hodl(hodlerAddr, { value: hodlValue })).wait();
+    const r1 = await (await autohodlContract.connect(hodler).hodl(hodlerAddr, hodlTime, { value: hodlValue })).wait();
+    const r2 = await (await autohodlContract.connect(hodler).withdraw(hodlerAddr)).wait();
+
+    const balancePost = await provider.getBalance(hodlerAddr);
+    expect(balancePre.sub(balancePost).sub(r1.gasUsed.mul(r1.effectiveGasPrice)).sub(r2.gasUsed.mul(r2.effectiveGasPrice)).toNumber()).to.equal(hodlValue);
+  });
+
+  it("Should return money eventually", async function () {
+    const [owner, hodler] = await ethers.getSigners();
+
+    const autohodlFactory = await ethers.getContractFactory("Autohodl");
+    const autohodlContract = await autohodlFactory.deploy();
+
+    const hodlerAddr = await hodler.getAddress();
+    const hodlValue = 12345;
+    const hodlTime = 3;
+
+    const balancePre = await provider.getBalance(hodlerAddr);
+    const r1 = await (await autohodlContract.connect(hodler).hodl(hodlerAddr, hodlTime, { value: hodlValue })).wait();
+    await new Promise(resolve => setTimeout(resolve, (hodlTime + 1) * 1000));
     const r2 = await (await autohodlContract.connect(hodler).withdraw(hodlerAddr)).wait();
 
     const balancePost = await provider.getBalance(hodlerAddr);
